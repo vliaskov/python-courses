0d92500d10d6c4f383b8ea36a3ac485602d8527b
Vasilis Liaskovitis <vliaskov@gmail.com>
2013-10-15 01:06:29 +0200

    rock, paper, scissors, lizard, spock

---
+++ rpsls.py
@@ -0,0 +1,29 @@
+choices = ["rock", "paper", "scissors", "lizard", "spock"]
+
+def name_to_number(name):
+  if name in choices:
+    return choices.index(name)
+  else:
+     return "invalid choice"
+
+
+def number_to_name(number):
+  if number < len(choices):
+    return choices[number]
+  else:
+    return "invalid choice"  
+
+def rpsls(number):
+  rand.numaj
+  if number < len():
+    return s[]
+  else:
+    return skata[]
+
+
+def online():
+  retun
+
+print name_to_number("spock")
+print number_to_name(3)
+
